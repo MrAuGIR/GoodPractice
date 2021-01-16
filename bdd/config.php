cc0bc9f8c4c9b3b6f6c1d13e449c2d1e84d978d1
MrAuGir <augirard17@gmail.com>
2021-01-17 00:03:17 +0100

    initial project version

---
+++ bdd/config.php
@@ -0,0 +1,16 @@
+<?php
+
+define('DB_NAME','bonne_pratique');
+
+define('USER_NAME', 'root');
+
+define('HOST', 'localhost');
+
+define('PASSWORD', '');
+
+define('LEVEL_ADMIN', 'administrateur');
+
+define('LEVEL_EDITEUR','editeur');
+
+
+?>
