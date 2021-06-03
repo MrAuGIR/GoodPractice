6422a6bf5b43aa5842e4ba4372f179222ff2bfde
MrAuGir <augirard17@gmail.com>
2021-06-03 21:01:57 +0200

    path update

--- class/Render.php
+++ class/Render.php
@@ -5,7 +5,7 @@ class Render{
 
 
     public static function render(string $path, array $var){
-        $loader = new \Twig\Loader\FilesystemLoader('/homepages/27/d812845851/htdocs/projets/GoodPractice/Templates/');
+        $loader = new \Twig\Loader\FilesystemLoader('Templates/');
         $twig = new \Twig\Environment($loader);
 
         echo $twig->render($path.'.html.twig', $var);
