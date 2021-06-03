a764186cfef30294e06d921617bb909614777141
MrAuGir <augirard17@gmail.com>
2021-06-03 21:05:51 +0200

    template update

--- class/Render.php
+++ class/Render.php
@@ -5,7 +5,7 @@ class Render{
 
 
     public static function render(string $path, array $var){
-        $loader = new \Twig\Loader\FilesystemLoader('Templates/');
+        $loader = new \Twig\Loader\FilesystemLoader('../Templates');
         $twig = new \Twig\Environment($loader);
 
         echo $twig->render($path.'.html.twig', $var);
